12bd66c3726792b581c59894069aa8ba00b0fbe2
ahmed-bs <medamed777@gmail.com>
2024-02-02 14:03:18 +0100

    starting team

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -13,7 +13,7 @@ export class UserComponent implements OnInit {
   listUser: User[] = [];
   newUser: User = new User();
 
-  constructor( private userService: UserService) { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
     this.getAllUser();
@@ -21,7 +21,7 @@ export class UserComponent implements OnInit {
 
   getAllUser() {
     this.userService.getAllUsers().subscribe(
-      data=> {this.listUser = data;},
+      data => { this.listUser = data; },
       error => {
         console.error(error);
       }
@@ -29,90 +29,88 @@ export class UserComponent implements OnInit {
   }
 
   addUser(): void {
-this.userService.createUser(this.newUser).subscribe(
-  data => {
-    console.log('User Added succesfully:', data);
-    this.getAllUser();
-    Swal.fire({
-      icon: 'success',
+    this.userService.createUser(this.newUser).subscribe(
+      data => {
+        console.log('User Added succesfully:', data);
+        this.getAllUser();
+        Swal.fire({
+          icon: 'success',
           title: 'success !',
           timer: 1500,
           showConfirmButton: false,
-    })
-    $('#AddModal').modal('hide');
-  },
-  error => {
-    Swal.fire({
-      icon: 'error',
-      title: error.error.message,
-      timer: 1500,
-      showConfirmButton: false,
-    })
-    console.error('Error adding User:', error);
+        })
+        $('#AddModal').modal('hide');
+      },
+      error => {
+        Swal.fire({
+          icon: 'error',
+          title: error.error.message,
+          timer: 1500,
+          showConfirmButton: false,
+        })
+        console.error('Error adding User:', error);
+      }
+    );
+    this.newUser = new User();
   }
-);
 
+
+
+  openEditModel(user: User) {
+    this.newUser.id = user.id;
+    this.newUser.username = user.username;
+    this.newUser.firstName = user.firstName;
+    this.newUser.lastName = user.lastName;
+    this.newUser.dateOfBirth = user.dateOfBirth;
+    this.newUser.email = user.email;
+    this.newUser.password = user.password;
   }
 
-
-
-  openEditModel(user:User)
-{
-  this.newUser.id=user.id;
-  this.newUser.username = user.username;
-  this.newUser.firstName = user.firstName;
-  this.newUser.lastName = user.lastName;
-  this.newUser.dateOfBirth = user.dateOfBirth;
-  this.newUser.email = user.email;
-
-  this.newUser.password = user.password;
-  
-}
-
-editUser(): void {
-  this.userService.updateUser(this.newUser.id,this.newUser).subscribe(data => {
-    this.getAllUser();
-    Swal.fire({
-      icon: 'success',
-      title:'Modified !',
-      timer: 1500,
-      showConfirmButton: false,
-    })
-    $('#EditModal').modal('hide');
-  },error=>{
-    Swal.fire({
-      icon: 'error',
-      title: error.error.message,
-      timer: 1500,
-      showConfirmButton: false,
-    })
-  }) 
-}
-
-
-DeleteUser(id:number): void {
-  this.userService.deleteUser(id).subscribe(
-    data => {
-      console.log('User Deleted successfully:', data);
+  editUser(): void {
+    this.userService.updateUser(this.newUser.id, this.newUser).subscribe(data => {
+      this.getAllUser();
       Swal.fire({
         icon: 'success',
-        title: 'Deleted !',
+        title: 'Modified !',
         timer: 1500,
         showConfirmButton: false,
       })
-      this.listUser = this.listUser.filter(user => user.id !== id);
-    },
-    error => {
+      $('#EditModal').modal('hide');
+    }, error => {
       Swal.fire({
         icon: 'error',
         title: error.error.message,
         timer: 1500,
         showConfirmButton: false,
       })
-      console.error('Error Deleting user:', error);
-    }
-  );
-}
+    })
+    this.newUser = new User();
 
+  }
+
+
+  DeleteUser(id: number): void {
+    this.userService.deleteUser(id).subscribe(
+      data => {
+        console.log('User Deleted successfully:', data);
+        Swal.fire({
+          icon: 'success',
+          title: 'Deleted !',
+          timer: 1500,
+          showConfirmButton: false,
+        })
+        this.listUser = this.listUser.filter(user => user.id !== id);
+      },
+      error => {
+        Swal.fire({
+          icon: 'error',
+          title: error.error.message,
+          timer: 1500,
+          showConfirmButton: false,
+        })
+        console.error('Error Deleting user:', error);
+      }
+    );
+  }
 
 }
