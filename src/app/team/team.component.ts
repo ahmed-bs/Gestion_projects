b3613236acbcc78abc71ddc31b6eabe674bd4ca4
ahmed-bs <medamed777@gmail.com>
2024-02-24 19:22:43 +0100

    teams

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -3,17 +3,28 @@ import { Team } from '../models/team';
 import { TeamService } from '../services/team.service';
 import { User } from '../models/user';
 import { UserService } from '../services/user.service';
-
+import Swal from 'sweetalert2';
+declare var $: any;
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
   styleUrls: ['./team.component.css']
 })
 export class TeamComponent implements OnInit {
+
   listUser: User[] = [];
   teamList: Team[] = [];
   newTeam: Team = new Team();
-  constructor( private teamService: TeamService,private userService: UserService) { }
+
+  constructor( private teamService: TeamService,private userService: UserService) { 
+    this.list = 
+    [
+      {name :'India',checked : false},
+      {name :'US',checked : false},
+      {name :'China',checked : false},
+      {name :'France',checked : false}
+    ]
+  }
 
   ngOnInit() {
     this.getAllTeams();
@@ -23,14 +34,15 @@ export class TeamComponent implements OnInit {
 
   getAllTeams() {
     this.teamService.getAllTeams().subscribe(
-      data => {this.teamList = data;
-      console.log("this is teams list :",data);
+      data => {
+        this.teamList = data;
       },
       error => {
         console.error(error);
       }
     )
   }
+
   getAllUser() {
     this.userService.getAllUsers().subscribe(
       data => { this.listUser = data; },
@@ -41,8 +53,39 @@ export class TeamComponent implements OnInit {
   }
 
   addTeam(){
-
+    this.newTeam
+    console.log("this is wurt : ",this.newTeam)
+    // this.teamService.createTeam(this.newTeam).subscribe(
+    //   data => {
+    //     console.log('Team added successfully:', data);
+    //     this.getAllTeams();;
+    //     Swal.fire({
+    //       icon: 'success',
+    //       title: 'success !',
+    //       timer: 1500,
+    //       showConfirmButton: false,
+    //     })
+    //     $('#AddModal').modal('hide');
+    //   },
+    //   error => {
+    //     Swal.fire({
+    //       icon: 'error',
+    //       title: error.error.message,
+    //       timer: 1500,
+    //       showConfirmButton: false,
+    //     })
+    //     console.error('Error adding Team:', error);
+    //   }
+    // );
   }
+  list! : any[];
+  
 
+  shareCheckedList(item:any[]){
+    console.log(item);
+  }
+  shareIndividualCheckedList(item:{}){
+    console.log(item);
+  }
 
 }
