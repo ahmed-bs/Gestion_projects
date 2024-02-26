7b9b816ea96e79a8ab2958343efd5a48f4b73f58
ahmed-bs <medamed777@gmail.com>
2024-02-26 18:30:02 +0100

    login

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -1,41 +1,47 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, catchError, throwError } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { Project } from '../models/project';
 import { environment } from 'src/environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectService {
-  private baseUrl = environment.Api +"projects"; 
+  private baseUrl = environment.Api + "projects";
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem('token')}`
+    })
+  };
+
   private handleError(error: any): Observable<never> {
     console.error('An error occurred:', error);
     return throwError('Something went wrong, please try again later.');
   }
-  constructor(private http: HttpClient) {}
+
+  constructor(private http: HttpClient) { }
 
   getAllProjects(): Observable<Project[]> {
-    return this.http.get<Project[]>(`${this.baseUrl}/all`)
-    .pipe(catchError(this.handleError));
+    return this.http.get<Project[]>(`${this.baseUrl}/all`, this.httpOptions)
+      .pipe(catchError(this.handleError));
   }
 
-
-
-
   getProjectById(id: number): Observable<Project> {
-    return this.http.get<Project>(`${this.baseUrl}/getById/${id}`);
+    return this.http.get<Project>(`${this.baseUrl}/getById/${id}`, this.httpOptions);
   }
 
   createProject(project: Project): Observable<any> {
-    return this.http.post(`${this.baseUrl}/save`, project);
+    return this.http.post(`${this.baseUrl}/save`, project, this.httpOptions);
   }
 
   updateProject(project: Project): Observable<any> {
-    return this.http.put(`${this.baseUrl}/update/${project.id}`, project);
+    return this.http.put(`${this.baseUrl}/update/${project.id}`, project, this.httpOptions);
   }
 
   deleteProject(id: number): Observable<any> {
-    return this.http.delete(`${this.baseUrl}/delete/${id}`);
+    return this.http.delete(`${this.baseUrl}/delete/${id}`, this.httpOptions);
   }
-
 }
--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../services/login.service';
 
 @Component({
   selector: 'app-top-bar',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./top-bar.component.css']
 })
 export class TopBarComponent implements OnInit {
+  username!:string;
 
-  constructor() { }
+  constructor(private loginService: LoginService) { }
 
   ngOnInit(): void {
+    this.username = localStorage.getItem("userName")!;
   }
+
+
+
 
 }
